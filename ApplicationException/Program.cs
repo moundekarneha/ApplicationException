4f10b77c2e657d5ec2bcfdc78a8689d00979ffaa
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 17:13:39 +0530

    Add project files.

---
+++ ApplicationException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ApplicationExceptionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter number");
+                int a = int.Parse(Console.ReadLine());
+                if (a==5)
+                {
+                    throw new ApplicationException();
+                }
+            }
+            catch (ApplicationException e)
+            {
+                Console.WriteLine(e.Message);
+               
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
